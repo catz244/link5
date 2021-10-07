d23b4c5228ba6bc795e1b7a0ce3cd5d3e44c8f72
catz244 <78543753+catz244@users.noreply.github.com>
2021-10-07 18:03:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+width : 300,
+height : 300,
+image_format : 'png',
+png_quality : 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+
+Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML = '<img id = "capture_img" src ="' + data_uri + '" ></img>';
+});
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Py63SogvL/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log("model is loaded");
+}
+
+function check(){
+    img = document.getElementById('capture_img');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+       console.log(results);
+       document.getElementById("result_object").innerHTML = results[0].label;
+       document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
